57de874e969bb9a5d38b0541def36c7ecc19bbdb
Adrian Sudbury <sudburya@gmail.com>
2021-09-14 10:19:22 +0100

    Update to Links display.

--- src/components/launchLinks.tsx
+++ src/components/launchLinks.tsx
@@ -2,24 +2,57 @@ import React from 'react';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import { makeStyles } from '@material-ui/core';
 import { Links } from '../types';
+
+const useStyles = makeStyles(() => ({
+  link: {
+    textDecoration: 'underline'
+  }
+}));
 
 interface LinksProps {
   links: Links;
 }
 
 export default function LaunchLinks({ links }: LinksProps): JSX.Element {
+  const classes = useStyles();
+
   return (
     <div>
       <List>
         <ListItem>
-          <ListItemText primary="Article" secondary={links.article_link} />
+          <ListItemText primary="Article" />
+        </ListItem>
+        <ListItem
+          button
+          component="a"
+          href={links.article_link}
+          className={classes.link}
+        >
+          <ListItemText primary={links.article_link} />
         </ListItem>
         <ListItem>
-          <ListItemText primary="Wikipedia" secondary={links.wikipedia} />
+          <ListItemText primary="Wikipedia" />
+        </ListItem>
+        <ListItem
+          button
+          component="a"
+          href={links.wikipedia}
+          className={classes.link}
+        >
+          <ListItemText primary={links.wikipedia} />
         </ListItem>
         <ListItem>
-          <ListItemText primary="Video" secondary={links.video_link} />
+          <ListItemText primary="Video" />
+        </ListItem>
+        <ListItem
+          button
+          component="a"
+          href={links.video_link}
+          className={classes.link}
+        >
+          <ListItemText primary={links.video_link} />
         </ListItem>
       </List>
     </div>
