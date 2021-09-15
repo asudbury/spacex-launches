065680732d629eeb0ef650b96050647cd02b7bd4
Adrian Sudbury <sudburya@gmail.com>
2021-09-15 19:27:16 +0100

    Fixed duplicate code issue on sonarcloud

--- src/components/launches.test.tsx
+++ src/components/launches.test.tsx
@@ -1,54 +1,10 @@
 import React from 'react';
-import ReactRouter from 'react-router';
 import { Provider } from 'react-redux';
-import configureStore from 'redux-mock-store';
-import thunk from 'redux-thunk';
 import { render } from '@testing-library/react';
 import Launches from './launches';
-import launchesTestData from '../testData/launchesTestData';
+import store from '../testData/mockStore';
 
-test('Launch', () => {
-  const middlewares = [thunk];
-  const mockStore = configureStore(middlewares);
-
-  const initialState = {
-    spaceXApi: {
-      queries: {
-        'getLaunches("")': {
-          status: 'fulfilled',
-          endpointName: 'getLaunches',
-          requestId: '9CsdcrS3ZHiWro4rXE0WT',
-          originalArgs: '',
-          startedTimeStamp: 1631720394141,
-          data: launchesTestData,
-          mutations: {},
-          provided: {},
-          subscriptions: {
-            'getLaunches("")': {
-              '9CsdcrS3ZHiWro4rXE0WT': {
-                pollingInterval: 0
-              }
-            }
-          }
-        }
-      },
-      config: {
-        online: true,
-        focused: true,
-        middlewareRegistered: true,
-        refetchOnFocus: false,
-        refetchOnReconnect: false,
-        refetchOnMountOrArgChange: false,
-        keepUnusedDataFor: 60,
-        reducerPath: 'spaceXApi'
-      }
-    }
-  };
-
-  const store = mockStore(initialState);
-
-  jest.spyOn(ReactRouter, 'useParams').mockReturnValue({ launchId: '60' });
-
+test('Launches', () => {
   const { container, getByTestId } = render(
     <Provider store={store}>
       <Launches />
